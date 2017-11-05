e8a32932aceba12e045abfc5e2d7c6c31cebe3a3
Dima Bunak <bunakdima1998@gmail.com>
2017-11-05 21:44:21 +0200

    Ready Grid

--- SatronusNext/ProgramWindow.xaml.cs
+++ SatronusNext/ProgramWindow.xaml.cs
@@ -11,9 +11,21 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SatronusNext.viewModel;
 
 namespace SatronusNext
 {
+    public class Event
+    {
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public Event() { Time = DateTime.Now; }
+        public Event(string name,DateTime time)
+        {
+            Time = time;
+            Name = name;
+        }
+    }
     /// <summary>
     /// Логика взаимодействия для ProgramWindow.xaml
     /// </summary>
@@ -22,6 +34,25 @@ namespace SatronusNext
         public ProgramWindow()
         {
             InitializeComponent();
+            Loaded += ProgramWindow_Loaded;
+        }
+
+        private void ProgramWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Event[] mass = new Event[]{
+            new Event("Будильник 1",DateTime.Now), 
+            new Event("Будильник 2",DateTime.Now), 
+            new Event("Будильник 3", DateTime.Now),
+             new Event("Будильник 1",DateTime.Now),
+            new Event("Будильник 2",DateTime.Now),
+            new Event("Будильник 3", DateTime.Now),
+             new Event("Будильник 1",DateTime.Now),
+            new Event("Будильник 2",DateTime.Now),
+            new Event("Будильник 3", DateTime.Now)
+
+                };
+            DataContext = new EventViewModel(mass);
+
         }
     }
 }
